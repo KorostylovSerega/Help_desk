a48440b9f9ff5454750665973f4988ce8fef3629
KorostylovSerega <armfighter89@gmail.com>
2022-12-22 10:39:07 +0200

    update

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -3,10 +3,8 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    username = None
     first_name = models.CharField('first name', max_length=150)
     last_name = models.CharField('last name', max_length=150)
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
