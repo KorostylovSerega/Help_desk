40ced68e09edbc331428d1f700e8d5866477cd19
serega <armfighter89@gmail.com>
2022-12-25 23:48:28 +0200

    update

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -1,14 +1,17 @@
 from django.contrib.auth import views
 from django.urls import path
 
-from helpdesk.views import UserCreateView, TicketCreateView, TicketListView, TicketDetailView, CommentCreateView
+from helpdesk.views import UserCreateView, TicketCreateView, TicketUpdateView,\
+    TicketListView, TicketDetailView, CommentCreateView, ChangeTicketStatusView
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
     path('', TicketListView.as_view(), name='home'),
-    path('ticket/<int:pk>/', TicketDetailView.as_view(), name='ticket_detail'),
+    path('ticket/<int:pk>/', TicketDetailView.as_view(), name='detail_ticket'),
     path('add-ticket/', TicketCreateView.as_view(), name='add_ticket'),
+    path('update-ticket/<int:pk>/', TicketUpdateView.as_view(), name='update_ticket'),
+    path('update-ticket-status/<int:pk>/<int:status>/', ChangeTicketStatusView.as_view(), name='change_status'),
     path('add-comment/<int:pk>', CommentCreateView.as_view(), name='add_comment'),
 ]
--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 
 from helpdesk.forms import UserCreateForm, CommentCreateForm
 from helpdesk.models import CustomUser, Ticket
@@ -37,6 +38,21 @@ class TicketCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+class TicketUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
+    model = Ticket
+    fields = ['description', 'priority']
+    template_name = 'helpdesk/ticket_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('detail_ticket', kwargs={'pk': self.object.pk})
+
+    def form_valid(self, form):
+        if self.object.status != 1:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().form_valid(form)
+
+
 class TicketListView(LoginRequiredMixin, ListView):
     login_url = reverse_lazy('login')
     model = Ticket
@@ -55,7 +71,35 @@ class TicketDetailView(LoginRequiredMixin, DetailView):
     login_url = reverse_lazy('login')
     model = Ticket
     template_name = 'helpdesk/ticket_detail.html'
-    extra_context = {'form': CommentCreateForm}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.object.status == 1:
+            context['comment_form'] = CommentCreateForm()
+        return context
+
+
+class ChangeTicketStatusView(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
+    model = Ticket
+    fields = ['status']
+    template_name = 'helpdesk/ticket_detail.html'
+
+    def get_success_url(self):
+        return reverse_lazy('detail_ticket', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_status = self.object.status
+        changed_status = self.kwargs.get('status')
+        if current_status in [1, 3] and changed_status in [2, 3, 4]:
+            context['status_form'] = CommentCreateForm()
+        return context
+
+    # def form_valid(self, form):
+    #     if self.object.status != 1:
+    #         return HttpResponseRedirect(self.get_success_url())
+    #     return super().form_valid(form)
 
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
@@ -71,3 +115,6 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         comment.author = author
         comment.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('detail_ticket', kwargs={'pk': self.object.ticket.id})
